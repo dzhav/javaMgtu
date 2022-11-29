20ae7d46123f38d450856a869a6782d2d7eb7acf
dzhav <fataliev.ds@phystech.edu>
2022-11-29 10:53:11 +0300

    fddsf

---
+++ Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+class Main {
+  public static void main(String[] args) {
+    int count = 0;
+    int a, b = 1, c = 2;
+    double r = 1.4;
+    float f = 1.4f;
+    boolean trueFalse = true;
+    String str = "Stroka"
+    char cc = 'r';
+    char ccc = 143;
+
+
+    if (true){
+      System.out.println("hello, world");
+    }
+    else{
+      
+    }
+  }
+
+}
